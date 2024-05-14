fb90d9217762f7d939ace13306693a42bdd6a4bd
sywc670 <670886335@qq.com>
2024-05-14 09:05:19 +0800

    feat: add Group

--- geecache/cache.go
+++ geecache/cache.go
@@ -1,1 +1,36 @@
 package geecache
+
+import (
+	"sync"
+
+	"github.com/sywc670/gee/geecache/lru"
+)
+
+// support concurrent
+type cache struct {
+	mu         sync.Mutex
+	cacheBytes int64
+	lru        *lru.Cache
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+	v, ok := c.lru.Get(key)
+	if ok {
+		return v.(ByteView), ok
+	}
+	return
+}
---
+++ geecache/geecache.go
@@ -0,0 +1,79 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+type Getter interface {
+	Get(string) ([]byte, error)
+}
+
+type GetterFunc func(string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+type Group struct {
+	mainCache *cache
+	getter    Getter
+	name      string
+}
+
+func NewGroup(name string, cacheSize int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: &cache{cacheBytes: cacheSize},
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	groups[name] = g
+
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+	g := groups[name]
+
+	return g
+}
+
+func (g *Group) Get(key string) (value ByteView, err error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+	if value, ok := g.mainCache.get(key); ok {
+		log.Println("[geecache] hit!")
+		return value, nil
+	}
+	log.Println("[geecache] miss!")
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	b, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	// clone
+	bv := ByteView{clonebytes(b)}
+	g.populateCache(key, bv)
+	return bv, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
